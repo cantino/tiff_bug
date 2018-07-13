34bd2a9627eb108370e5e21ea6ee6eed0f2cb9e5
Andrew Cantino <cantino@users.noreply.github.com>
2018-07-12 17:40:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+```bash
+go build
+./tiff_bug
+```
+
+Open the resulting `out-compressed.tiff` and `out-noncompressed.tiff` in Mac OS X Preview. The ~80MB uncompressed works fine, the compressed gives an error:
---
+++ tiff_bug.go
@@ -0,0 +1,35 @@
+package main                                                                                    
+
+import (
+  "image"
+  "image/color"
+  "math"
+  "log"
+  "os"
+
+  "golang.org/x/image/tiff"
+)
+
+func main() {
+  width, height := 4104, 2736
+
+	i := image.NewRGBA64(image.Rect(0, 0, width, height))
+
+  for y := 0; y < height; y++ {
+    for x := 0; x < width; x++ {
+      i.Set(x, y, color.RGBA64{R:math.MaxUint16, G:math.MaxUint16, B:math.MaxUint16, A:math.MaxUint16})
+    }
+  }
+
+	outputFile, err := os.Create("out-compressed.tiff")
+	if err != nil {
+    log.Fatal(err)
+	}
+	tiff.Encode(outputFile, i, &tiff.Options{Compression: tiff.Deflate})
+
+	outputFile, err = os.Create("out-noncompressed.tiff")
+	if err != nil {
+    log.Fatal(err)
+	}
+	tiff.Encode(outputFile, i, &tiff.Options{})
+}
